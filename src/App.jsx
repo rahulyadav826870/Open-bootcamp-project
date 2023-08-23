f2678ccc7570c665c74b99526099e51d8c902473
rahulyadav826870 <rahulyadav826870@gmail.com>
2023-08-23 13:39:59 +0530

    basic portfolio

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+
+import './App.css'
+import Navabar from './components/Navabar'
+
+export default function App() {
+  return (
+    <Navabar />
+  )
+}
---
+++ src/components/Navabar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Navabar() {
+  return (
+    <>
+      <div className="p-6 bg-blue-200">
+        <nav className="flex justify-between">
+       <p className="first-letter:text-[red] ">Rahul Yadav</p> 
+
+          <ol className="flex justify-between px-1 text-[red] md:text-[20px]">
+            <li className="mx-4">Home </li>
+            <li className="mx-4">Abouttt </li>
+            <li className="mx-4">Contact </li>
+          </ol>
+        </nav>
+      </div>
+    </>
+  );
+}
